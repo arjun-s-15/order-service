16bf55eff45d41244e3e561e6cf16cc39e48f2aa
arjun-s-15 <iamarjun9003@gmail.com>
2026-06-30 23:03:17 +0530

    added Webclient       Service Discovery-Eureka       API Gateway       Oauth 2.0 - Keycloak       Swagger OpenAPI,circuit breaker

--- src/main/java/com/AJ_15/order_service/service/OrderService.java
+++ src/main/java/com/AJ_15/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.AJ_15.order_service.service;
 
+import com.AJ_15.order_service.dto.InventoryResponse;
 import com.AJ_15.order_service.dto.OrderLineItemsDto;
 import com.AJ_15.order_service.dto.OrderRequest;
 import com.AJ_15.order_service.model.Order;
@@ -7,16 +8,21 @@ import com.AJ_15.order_service.model.OrderLineItems;
 import com.AJ_15.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
     public final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
-    public void createOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -26,7 +32,25 @@ public class OrderService {
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode)
+                .toList();
+//      Call inventory service and place order if item in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> {
+                            return uriBuilder.queryParam("skuCode", skuCodes).build();
+                        })
+                        .retrieve()
+                                .bodyToMono(InventoryResponse[].class)
+                                        .block();
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                                            .allMatch(InventoryResponse::getIsInStock);
+        if(allProductsInStock) {
+            orderRepository.save(order);
+            return "Order placed successfully!";
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
